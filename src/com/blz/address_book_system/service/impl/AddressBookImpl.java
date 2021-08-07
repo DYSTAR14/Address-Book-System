1f5abbcba9ec84cccb4e3fc886656d49e404f431
DYSTAR14 <ydeepak141097@gmail.com>
2021-08-07 18:01:25 +0530

    Refactor: Multiple Person Details Can Add

--- src/com/blz/address_book_system/service/impl/AddressBookImpl.java
+++ src/com/blz/address_book_system/service/impl/AddressBookImpl.java
@@ -14,45 +14,23 @@ public class AddressBookImpl implements IAddressBook{
 	static final Scanner SC=new Scanner(System.in);
 	@Override
 	public void addContact() {
-		// TODO Auto-generated method stub
-//		System.out.println("Enter First Name");
-//		firstName=SC.nextLine();
-//		System.out.println("Enter Second Name");
-//		lastName=SC.nextLine();
-//		System.out.println("Enter Address");
-//		address=SC.nextLine();
-//		System.out.println("Enter City Name");
-//		city=SC.nextLine();
-//		System.out.println("Enter State Name");
-//		state=SC.nextLine();
-//		System.out.println("Enter Email");
-//		email=SC.nextLine();
-//		System.out.println("Enter Zip Code");
-//		zip=SC.nextLine();
-//		System.out.println("Enter Phone Number");
-//		phone=SC.nextLine();
-		firstName="Deepak";lastName="Yadav";address="Empress Thane";city="Thane";state="Maharashtra";
-		email="ydeepak@gmail.com";zip = "400005";phone="9823563242";
+		System.out.println("Enter First Name");
+		firstName=SC.next();
+		System.out.println("Enter Second Name");
+		lastName=SC.next();
+		System.out.println("Enter Address");
+		address=SC.next();
+		System.out.println("Enter City Name");
+		city=SC.next();
+		System.out.println("Enter State Name");
+		state=SC.next();
+		System.out.println("Enter Email");
+		email=SC.next();
+		System.out.println("Enter Zip Code");
+		zip=SC.next();
+		System.out.println("Enter Phone Number");
+		phone=SC.next();
 		contactDetails=new ContactDetails(firstName, lastName, address, city, state, email, zip, phone);
-//		System.out.println(contactDetails);
-		contactList.add(contactDetails);
-
-		firstName="Sarang";lastName="Potpite";address="Pune nagar";city="Pune";state="Maharashtra";
-		email="sarang@gmail.com";zip = "564305";phone="9823326657";
-		contactDetails=new ContactDetails(firstName, lastName, address, city, state, email, zip, phone);
-//		System.out.println(contactDetails);
-		contactList.add(contactDetails);
-		
-		firstName="Gaurav";lastName="Yadav";address="Empress Thane";city="Thane";state="Maharashtra";
-		email="Gaurav@gmail.com";zip = "400005";phone="9823565436";
-		contactDetails=new ContactDetails(firstName, lastName, address, city, state, email, zip, phone);
-//		System.out.println(contactDetails);
-		contactList.add(contactDetails);
-		
-		firstName="Ronak";lastName="Jain";address="Gokul Nagar";city="Thane";state="Maharashtra";
-		email="Ronak@gmail.com";zip = "400005";phone="9825674567";
-		contactDetails=new ContactDetails(firstName, lastName, address, city, state, email, zip, phone);
-//		System.out.println(contactDetails);
 		contactList.add(contactDetails);
 		System.out.println("*** *** LIST OF DETAILS *** ***");
 		for (int i = 0; i < contactList.size(); i++)
@@ -60,36 +38,32 @@ public class AddressBookImpl implements IAddressBook{
 	}
 	@Override
 	public void editContact() {
-		// TODO Auto-generated method stub
 		System.out.println("Enter the Username");
-		String searchName=SC.nextLine();
+		String searchName=SC.next();
 		int count=0;
 		for (ContactDetails user :contactList ) {
 			count++;
 			if(user.getFirstName()!=null && user.getFirstName().contains(searchName)) {
-				//System.out.println(i.getFirstName() + " ");
 				count=count-1;
 				System.out.println(user.toString());
-//				System.out.println(contactList.get(count));
 				System.out.println("Please Enter the Same Detail or Change It");
 				System.out.println("First Name is '"+user.getFirstName()+"' Edit");
-				firstName=SC.nextLine();
+				firstName=SC.next();
 				System.out.println("Second Name is '"+user.getLastName()+"' Edit");
-				lastName=SC.nextLine();
+				lastName=SC.next();
 				System.out.println("Address is '"+user.getAddress()+"' Edit");
-				address=SC.nextLine();
+				address=SC.next();
 				System.out.println("City Name is '"+user.getCity()+"' Edit");
-				city=SC.nextLine();
+				city=SC.next();
 				System.out.println("State Name is '"+user.getState()+"' Edit");
-				state=SC.nextLine();
+				state=SC.next();
 				System.out.println("Email is '"+user.getEmail()+"' Edit");
-				email=SC.nextLine();
+				email=SC.next();
 				System.out.println("Zip Code is '"+user.getZip()+"' Edit");
-				zip=SC.nextLine();
+				zip=SC.next();
 				System.out.println("Phone Number is '"+user.getPhone()+"' Edit");
-				phone=SC.nextLine();
+				phone=SC.next();
 				contactDetails=new ContactDetails(firstName, lastName, address, city, state, email, zip, phone);
-//				System.out.println(contactDetails);
 				contactList.set(count,contactDetails);
 				System.out.println("*** *** LIST OF DETAILS *** ***");
 				for (int j = 0; j < contactList.size(); j++) {
@@ -101,14 +75,13 @@ public class AddressBookImpl implements IAddressBook{
 	}
 	@Override
 	public void deleteContact() {
-		// TODO Auto-generated method stub
 		System.out.println("List Of Person Name");
 		for (ContactDetails i :contactList ) {
 	          System.out.println(i.getFirstName() + " ");
 		}
 		System.out.println();
 		System.out.println("Enter the Username");
-		String searchName=SC.nextLine();
+		String searchName=SC.next();
 		int count=0;
 		for (ContactDetails user :contactList ) {
 			count++;
